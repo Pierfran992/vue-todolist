191649a6b2d7fae0291f7f37717a27b1b33a4698
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-29 14:19:02 +0100

    stato sviluppo: - richiamo vuejs; - creato l'array contenente gli oggetti;

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,25 @@
-console.log("HELLO WORLD");
+// richiamo Vuejs
+const {createApp} = Vue;
+
+createApp({
+    data () {
+        return {
+            newAnime: '',
+            listAnime: [
+                {
+                    title: "Made in Abyss",
+                    done: false
+                },
+                {
+                    title: "High School DxD",
+                    done: false
+                },
+                {
+                    title: "Tokyo Ghoul",
+                    done: false
+                },
+            ]
+        }
+    }
+
+}).mount("#myapp");
